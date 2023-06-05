2247fb12ef9744ae7973dac08ac305e1c6f0c672
Eduard Sardyka <eds832@gmail.com>
2023-06-05 18:04:43 +0200

    task2 improved

--- task2/app.js
+++ task2/app.js
@@ -1,14 +1,14 @@
 // 1. Write a JavaScript function that takes an array of numbers as input and returns
 // the sum of all even numbers in the array.
 
-let func1 = function (arr) {
-  let sum = 0;
-  for (let elem of arr) {
-    if (elem % 2 == 0) {
-      sum += elem;
+const func1 = (arr) => {
+  return arr.reduce((acc, arg) => {
+    if (arg % 2 === 0) {
+      return acc + arg;
+    } else {
+      return acc;
     }
-  }
-  return sum;
+  }, 0);
 };
 
 console.log("func1([1,2,3,4,5,6,7]) = " + func1([1, 2, 3, 4, 5, 6, 7]));
@@ -16,8 +16,8 @@ console.log("func1([1,2,3,4,5,6,7]) = " + func1([1, 2, 3, 4, 5, 6, 7]));
 // 2. Write a JavaScript function that takes two input values and
 // returns the concatenation of the two values as a string.
 
-let func2 = function (val1, val2) {
-  return val1 + "" + val2;
+const func2 = function (val1, val2) {
+  return `${val1}${val2}`;
 };
 
 console.log("func2(1,'some value') = " + func2(1, "some value"));
@@ -28,7 +28,7 @@ console.log("func2(true,2) = " + func2(true, 2));
 // 3. Write a JavaScript function that checks if a given number is positive, negative,
 // or zero and returns the corresponding string: "Positive", "Negative", or "Zero".
 
-let func3 = (num) => {
+const func3 = (num) => {
   if (num > 0) return "Positive";
   if (num < 0) return "Negative";
   return "Zero";
@@ -41,7 +41,7 @@ console.log("func3(0) = " + func3(0));
 // 4. Write a JavaScript function that calculates the factorial
 // of a given number using a while loop.
 
-let func4 = (num) => {
+const func4 = (num) => {
   if (num < 0 || num % 1 !== 0) return undefined;
   let result = 1;
   let current = num;
@@ -57,7 +57,7 @@ console.log("func4(7) = " + func4(7));
 // 5. Create an object called person with properties name and age.
 // Add a method called greet that returns a greeting string using the name property.
 
-let person = {
+const person = {
   name: "John",
   age: 21,
   greet() {
@@ -70,22 +70,16 @@ console.log(person.greet());
 // 6. Create a constructor function called `Car` that takes parameters for `make`, `model`,
 // and `year`. Add a method called `getInfo` that returns a string containing the car's details.
 
-function Car(make, model, year) {
-  this.make = make;
-  this.model = model;
-  this.year = year;
-  this.getInfo = function () {
-    return (
-      "Car{make:" +
-      this.make +
-      ", model:" +
-      this.model +
-      ", year:" +
-      this.year +
-      "}"
-    );
-  };
+class Car {
+  constructor(make, model, year) {
+    this.make = make;
+    this.model = model;
+    this.year = year;
+    this.getInfo = function () {
+      return `Car{make:${this.make}, model:${this.model}, year:${this.year}`;
+    };
+  }
 }
 
-let car = new Car("Ford", "Mondeo", 2000);
+const car = new Car("Ford", "Mondeo", 2000);
 console.log(car.getInfo());
